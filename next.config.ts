71171fcacc5bfadfa922857b696abb3feebd2110
SKR Software <skrsoftwarecr@gmail.com>
2026-03-12 17:47:47 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://encrypt

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
