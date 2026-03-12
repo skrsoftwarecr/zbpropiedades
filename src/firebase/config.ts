194c1bb47491c6b924b50e4ee12e09e8a98b774c
SKR Software <skrsoftwarecr@gmail.com>
2026-03-12 17:16:17 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2459066959-1cb9b",
+  "appId": "1:688072260222:web:8d3ffd4791821b21ff762f",
+  "apiKey": "AIzaSyC-6_iXTIzk2nOdD-D8mTBCeQCmvijnMXw",
+  "authDomain": "studio-2459066959-1cb9b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "688072260222"
+};
