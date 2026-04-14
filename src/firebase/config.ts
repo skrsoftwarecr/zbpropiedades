f8ff680cf15ffe8071084af07e1abc5fb5006e25
SKR Software <skrsoftwarecr@gmail.com>
2026-04-14 13:50:42 +0000

    continúa este error de permisos, aun despues de abrirlos:
    Missing or in

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "zb-propiedades-33291448-88816",
   "appId": "1:177199407222:web:b9ad5646a3415351fa7f57",
   "apiKey": "AIzaSyCcsf98_rKc566-sbYo-XUCX2Ewy1YLUgw",
   "authDomain": "zb-propiedades-33291448-88816.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "177199407222"
+  "messagingSenderId": "177199407222",
+  "storageBucket": "zb-propiedades-33291448-88816.firebasestorage.app"
 };
